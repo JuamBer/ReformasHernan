30a63f59fa2af98720918e9a669895509519b40b
Juan Sáez García <contacto@juamber.com>
2021-03-11 20:09:46 +0100

    tel

--- js/email.js
+++ js/email.js
@@ -1,12 +1,16 @@
 function sendMail(params){
+  console.log("finmcion");
+
   var tempParams={
-    to_name:document.getElementById("to_name").value,
-    from_name:document.getElementById("from_name").value,
-    message:document.getElementById("msj").value
+    to_name: document.getElementById("to_name").value,
+    from_name: document.getElementById("from_name").value,
+    message: document.getElementById("msj").value
   };
 
   emailjs.send('gmail','template_4i1c0yj',tempParams)
   .then(function(res){
     console.log("succes",  res.status);
   })
+  console.log("semd");
+
 }
