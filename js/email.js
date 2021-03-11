38a7a209cca848f6aff2c6a083ce85695cd7e487
Juan Sáez García <contacto@juamber.com>
2021-03-11 18:12:41 +0100

    h

--- js/email.js
+++ js/email.js
@@ -1,11 +1,5 @@
-Email.send({
-    Host : "smtp.yourisp.com",
-    Username : "username",
-    Password : "password",
-    To : 'them@website.com',
-    From : "you@isp.com",
-    Subject : "This is the subject",
-    Body : "And this is the body"
-}).then(
-  message => alert(message)
-);
+function sendMail(params){
+  var tempParams={
+    from_name
+  } 
+}
